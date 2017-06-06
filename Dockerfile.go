b705ffefe4abd1a0775e49f5d2c031907de169e1
Simon Sawert <simon@sawert.se>
2017-06-06 11:12:19 +0200

    Initial commit

---
+++ Dockerfile.go
@@ -0,0 +1,13 @@
+FROM golang:alpine
+
+RUN apk add --no-cache \
+    bash \
+    git \
+    gcc \
+    musl-dev \
+    curl \
+    openssh \
+    mysql-client
+
+ENV GOPATH="/go" 
+WORKDIR /go/src/github.com/bombsimon/laundry-service
