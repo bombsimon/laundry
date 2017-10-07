4e2dd9788a587c16da0f43a24206cc580a8f8cf1
Simon Sawert <simon@sawert.se>
2017-10-07 09:36:44 +0200

    Docker now starts with MySQL data automatically

--- config/sql/01.schema.sql
+++ config/sql/01.schema.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS `laundry`;
 
 CREATE DATABASE `laundry`;
-USE `laundry;
+USE `laundry`;
 
 CREATE TABLE `booker` (
     id          INT PRIMARY KEY AUTO_INCREMENT,
@@ -27,8 +27,8 @@ CREATE TABLE `slots` (
 
 CREATE TABLE `slots_machines` (
     id          INT PRIMARY KEY AUTO_INCREMENT,
-    id_machines INT NOT NULL,
     id_slots    INT NOT NULL,
+    id_machines INT NOT NULL,
 
     FOREIGN KEY (id_machines) REFERENCES machines(id) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (id_slots)    REFERENCES slots(id) ON UPDATE CASCADE ON DELETE CASCADE,
@@ -48,7 +48,7 @@ CREATE TABLE `bookings` (
 CREATE TABLE `notification_types` (
     id          INT PRIMARY KEY AUTO_INCREMENT,
     name        VARCHAR(25) NOT NULL,
-    description VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE `notifications` (
@@ -60,18 +60,3 @@ CREATE TABLE `notifications` (
     FOREIGN KEY (id_notification_types) REFERENCES notification_types(id) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (id_bookings)           REFERENCES bookings(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
-
--- Test data
-/*
-INSERT INTO `booker` VALUES (1,'1001','Some User','some.email@domain.com',NULL,'1234'),(2,'1002','Another User',NULL,NULL,NULL);
-
-INSERT INTO `machines` VALUES (1,'Electrolux 1',1),(2,'Electrolux 2',1),(3,'Broken machine',0);
-
-INSERT INTO `slots` VALUES (1,'1','07:00:00','10:00:00'),(2,'1','10:00:00','14:00:00'),(3,'1','14:00:00','18:00:00'),(4,'1','18:00:00','22:00:00'),(5,'2','07:00:00','10:00:00'),(6,'3','07:00:00','10:00:00'),(7,'4','07:00:00','10:00:00'),(8,'5','07:00:00','10:00:00'),(9,'2','10:00:00','14:00:00'),(10,'3','10:00:00','14:00:00'),(11,'4','10:00:00','14:00:00'),(12,'5','10:00:00','14:00:00'),(13,'2','14:00:00','18:00:00'),(14,'3','14:00:00','18:00:00'),(15,'4','14:00:00','18:00:00'),(16,'5','14:00:00','18:00:00'),(17,'2','18:00:00','22:00:00'),(18,'3','18:00:00','22:00:00'),(19,'4','18:00:00','22:00:00'),(20,'5','18:00:00','22:00:00');
-
-INSERT INTO `slots_machines` VALUES (1,1,1),(3,1,2),(5,1,3),(7,1,4),(8,1,7),(2,2,1),(4,2,2),(6,2,3),(10,2,4),(9,2,7);
-
-INSERT INTO `bookings` VALUES (1,'2017-08-23',4,1),(2,'2017-09-12',7,1),(3,'2017-09-12',8,1);
-
-INSERT INTO `notification_types` VALUES (1,'on_release','Someone cancels their slot'),(2,'reminder','Before your slot start');
-*/
---
+++ config/sql/03.grants.sql
@@ -0,0 +1,2 @@
+-- User grants does only permit '%' which does not include 'localhost'
+GRANT ALL PRIVILEGES ON `laundry`.* TO 'laundry'@'localhost' IDENTIFIED BY 'laundry';
